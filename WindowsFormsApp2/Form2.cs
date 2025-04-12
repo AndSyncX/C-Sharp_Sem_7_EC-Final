da50f88156a1a3d8d011df2db886cb4b5901f7de
AndSyncX <fernandez.joe@gmail.comco>
2025-04-11 19:54:51 -0500

    Feat: Segunda Parte

---
+++ WindowsFormsApp2/Form2.cs
@@ -0,0 +1,45 @@
+﻿using RestSharp;
+using RestSharp.Serialization.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnObtener_Click(object sender, EventArgs e)
+        {
+            string url = txtUrl.Text;
+
+            var client = new RestClient(url);
+            var request = new RestRequest(Method.GET);
+
+            try
+            {
+                IRestResponse response = client.Execute(request);
+                txtRespuesta.Text = response.Content; // Muestra el HTML
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
